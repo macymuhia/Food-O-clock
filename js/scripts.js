d019c96bd8e452116f5a70d7e6225fd6517bdf1e
macymuhia <macymuhia@gmail.com>
2019-06-21 10:50:17 +0300

    Add jumbotron styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     //Preloader
-    $(window).on("load", function () {
+    $(window).on("load", function() {
         preloaderFadeOutTime = 2000;
 
         function hidePreloader() {
@@ -9,13 +9,75 @@ $(document).ready(function () {
             preloader.fadeOut(preloaderFadeOutTime);
         }
         hidePreloader();
+
+        // filter code
+
+
+        filterSelection("all")
+
+        function filterSelection(c) {
+
+            var x, i;
+
+            x = document.getElementsByClassName("column");
+
+            if (c == "all") c = "";
+
+            for (i = 0; i < x.length; i++) {
+
+                w3RemoveClass(x[i], "show");
+
+                if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+
+            }
+
+        }
+
+        function w3AddClass(element, name) {
+
+            var i, arr1, arr2;
+
+            arr1 = element.className.split(" ");
+
+            arr2 = name.split(" ");
+
+            for (i = 0; i < arr2.length; i++) {
+
+                if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+
+            }
+
+        }
+
+        function w3RemoveClass(element, name) {
+
+            var i, arr1, arr2;
+
+            arr1 = element.className.split(" ");
+
+            arr2 = name.split(" ");
+
+            for (i = 0; i < arr2.length; i++) {
+
+                while (arr1.indexOf(arr2[i]) > -1) {
+
+                    arr1.splice(arr1.indexOf(arr2[i]), 1);
+
+                }
+
+            }
+
+            element.className = arr1.join(" ");
+
+        }
+
     });
 
-//QRcode section
-function generateQR() {
-    var myinput = $ ("#myinput").val();
-    new QRCode(document.getElementById("qrcode"), myinput);
-}
+    //QRcode section
+    function generateQR() {
+        var myinput = $("#myinput").val();
+        new QRCode(document.getElementById("qrcode"), myinput);
+    }
 
 
     // This will style the form we currently have
@@ -26,35 +88,35 @@ function generateQR() {
         this.total = total;
     }
 
-    $("#show14").click(function(){
+    $("#show14").click(function() {
         $("#show24").hide();
         $("#show34").hide();
         $(".2").hide();
         $(".3").hide();
         $(".4").hide();
-        $(".1").animate({left:200, opacity:"show"}, 1500);
+        $(".1").animate({ left: 200, opacity: "show" }, 1500);
     })
 
-    $("#show24").click(function(){
+    $("#show24").click(function() {
         $("#show34").hide();
         $("#show14").hide();
         $(".1").hide();
         $(".3").hide();
         $(".4").hide();
-        $(".2").animate({left:200, opacity:"show"}, 1500);
+        $(".2").animate({ left: 200, opacity: "show" }, 1500);
     })
 
-    $("#show34").click(function(){
+    $("#show34").click(function() {
         $("#show24").hide();
         $("#show14").hide();
         $(".1").hide();
         $(".2").hide();
         $(".4").hide();
-        $(".3").animate({left:200, opacity:"show"}, 1500);
+        $(".3").animate({ left: 200, opacity: "show" }, 1500);
     })
 
-    $("#press").click(function () {
-        $(".4").animate({left:200, opacity:"show"}, 1500);
+    $("#press").click(function() {
+        $(".4").animate({ left: 200, opacity: "show" }, 1500);
         var fName = $("#food option:selected").text();
         var fPrice = $("#food option:selected").val();
         var fPlate = $("#plate option:selected").val();
@@ -63,14 +125,14 @@ function generateQR() {
         var bumaye = '<tr> <td id = "fooda">' + newFood.name + '</td>' + '<td id = "price">' + newFood.price + '</td>' + '<td id = "total">' + newFood.total + '</td></tr>'
         $("table tbody").append(bumaye);
 
-        $("#pressed").last().click(function () {
-            $('table thead th').each(function (i) {
+        $("#pressed").last().click(function() {
+            $('table thead th').each(function(i) {
                 calculateColumn(i);
             });
 
             function calculateColumn(index) {
                 var total = 0;
-                $('table tr').each(function () {
+                $('table tr').each(function() {
                     var value = parseInt($('td', this).eq(index).text());
                     if (!isNaN(value)) {
                         total += value;
@@ -81,59 +143,59 @@ function generateQR() {
         })
     })
 
-$("#press1").click(function () {
-    $(".4").animate({left:200, opacity:"show"}, 1500);
-    var fName1 = $("#food1 option:selected").text();
-    var fPrice1 = $("#food1 option:selected").val();
-    var fPlate1 = $("#plate1 option:selected").val();
-    var fTotal1 = fPlate1 * fPrice1;
-    var newFood1 = new food(fName1, fPrice1, fTotal1);
-    var bumaye1 = '<tr> <td id = "fooda">' + newFood1.name + '</td>' + '<td id = "price">' + newFood1.price + '</td>' + '<td id = "total">' + newFood1.total + '</td></tr>'
-    $("table tbody").append(bumaye1);
+    $("#press1").click(function() {
+        $(".4").animate({ left: 200, opacity: "show" }, 1500);
+        var fName1 = $("#food1 option:selected").text();
+        var fPrice1 = $("#food1 option:selected").val();
+        var fPlate1 = $("#plate1 option:selected").val();
+        var fTotal1 = fPlate1 * fPrice1;
+        var newFood1 = new food(fName1, fPrice1, fTotal1);
+        var bumaye1 = '<tr> <td id = "fooda">' + newFood1.name + '</td>' + '<td id = "price">' + newFood1.price + '</td>' + '<td id = "total">' + newFood1.total + '</td></tr>'
+        $("table tbody").append(bumaye1);
 
-    $("#pressed1").last().click(function () {
-        $('table thead th').each(function (i) {
-            calculateColumn(i);
-        });
-
-        function calculateColumn(index) {
-            var total = 0;
-            $('table tr').each(function () {
-                var value = parseInt($('td', this).eq(index).text());
-                if (!isNaN(value)) {
-                    total += value;
-                }
+        $("#pressed1").last().click(function() {
+            $('table thead th').each(function(i) {
+                calculateColumn(i);
             });
-            $('table tfoot td').eq(index).text('Total:' + total);
-        }
-    })
-});
 
-$("#press2").click(function () {
-    $(".4").animate({left:200, opacity:"show"}, 1500);
-    var fName2 = $("#food2 option:selected").text();
-    var fPrice2 = $("#food2 option:selected").val();
-    var fPlate2 = $("#plate2 option:selected").val();
-    var fTotal2 = fPlate2 * fPrice2;
-    var newFood2 = new food(fName2, fPrice2, fTotal2);
-    var bumaye2 = '<tr> <td id = "fooda">' + newFood2.name + '</td>' + '<td id = "price">' + newFood2.price + '</td>' + '<td id = "total">' + newFood2.total + '</td></tr>'
-    $("table tbody").append(bumaye2);
+            function calculateColumn(index) {
+                var total = 0;
+                $('table tr').each(function() {
+                    var value = parseInt($('td', this).eq(index).text());
+                    if (!isNaN(value)) {
+                        total += value;
+                    }
+                });
+                $('table tfoot td').eq(index).text('Total:' + total);
+            }
+        })
+    });
 
-    $("#pressed2").last().click(function () {
-        $('table thead th').each(function (i) {
-            calculateColumn(i);
-        });
+    $("#press2").click(function() {
+        $(".4").animate({ left: 200, opacity: "show" }, 1500);
+        var fName2 = $("#food2 option:selected").text();
+        var fPrice2 = $("#food2 option:selected").val();
+        var fPlate2 = $("#plate2 option:selected").val();
+        var fTotal2 = fPlate2 * fPrice2;
+        var newFood2 = new food(fName2, fPrice2, fTotal2);
+        var bumaye2 = '<tr> <td id = "fooda">' + newFood2.name + '</td>' + '<td id = "price">' + newFood2.price + '</td>' + '<td id = "total">' + newFood2.total + '</td></tr>'
+        $("table tbody").append(bumaye2);
 
-        function calculateColumn(index) {
-            var total = 0;
-            $('table tr').each(function () {
-                var value = parseInt($('td', this).eq(index).text());
-                if (!isNaN(value)) {
-                    total += value;
-                }
+        $("#pressed2").last().click(function() {
+            $('table thead th').each(function(i) {
+                calculateColumn(i);
             });
-            $('table tfoot td').eq(index).text('Total:' + total);
-        }
-    })
-});
+
+            function calculateColumn(index) {
+                var total = 0;
+                $('table tr').each(function() {
+                    var value = parseInt($('td', this).eq(index).text());
+                    if (!isNaN(value)) {
+                        total += value;
+                    }
+                });
+                $('table tfoot td').eq(index).text('Total:' + total);
+            }
+        })
+    });
 });
